989ff7b7dd7a3d86979740162e0e23ac6cbc083f
Panagiotis Moulos <moulos@fleming.gr>
2014-04-10 06:08:46 +0000

    Forgot to change version and date in metaseqR-package.R.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/metaseqR@88704 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/metaseqR-package.R
+++ R/metaseqR-package.R
@@ -4,8 +4,8 @@
 #' \tabular{ll}{
 #' Package: \tab metaseqR\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.99.5\cr
-#' Date: \tab 2014-03-31\cr
+#' Version: \tab 0.99.6\cr
+#' Date: \tab 2014-04-09\cr
 #' Depends: \tab R (>= 2.13.0), Biobase, BiocGenerics, rjson, biomaRt, utils,
 #' knitr, EDASeq, DESeq, edgeR, limma, NOISeq, baySeq, NBPSeq, MADAM, survcomp,
 #' brew, gplots, corrplot, GenomicRanges, Rsamtools, rtracklayer, Repitools,
